084a9c472cb2ecb5840babd96a79a004e4800a8a
Вадим <itamiomw@gmail.com>
2022-06-29 09:06:57 +0300

    added navigation between fragments

--- app/src/main/java/com/example/fastcalculatetraining/domain/models/GameSettings.kt
+++ app/src/main/java/com/example/fastcalculatetraining/domain/models/GameSettings.kt
@@ -1,10 +1,15 @@
 package com.example.fastcalculatetraining.domain.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import java.io.Serializable
+
+@Parcelize
 data class GameSettings(
     val gameTime: Int,
     val minCountOfRightAnswers: Int,
     val minPercentOfRightAnswers: Int,
     val maxSumValue: Int
-) {
+): Parcelable{
 
 }
--- app/src/main/java/com/example/fastcalculatetraining/presentation/WelcomeFragment.kt
+++ app/src/main/java/com/example/fastcalculatetraining/presentation/WelcomeFragment.kt
@@ -25,6 +25,16 @@ class WelcomeFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.buttonUnderstand.setOnClickListener {
+            launchChooseLevelFragment()
+        }
+    }
+
+    private fun launchChooseLevelFragment() {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, ChooseLevelFragment.newInstance())
+            .addToBackStack(null)
+            .commit()
     }
 
 }
